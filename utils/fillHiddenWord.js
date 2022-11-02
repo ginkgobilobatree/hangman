3b3ab7316092ef7ebadd8369cfb3dc82cc5b594b
Gino Massalski <72438321+ginkgobilobatree@users.noreply.github.com>
2022-11-02 21:45:00 +0100

    Update fillHiddenWord.js

--- utils/fillHiddenWord.js
+++ utils/fillHiddenWord.js
@@ -1,10 +1,10 @@
 function fillHiddenWord(finalHiddenWord, word, answer) {
-    let indeces = [];
+    let indeces = []; // store for indeces that matches the word's characters
     let result = '';
     for (let i = 0; i < word.length; i++) {
         if (word[i].toLowerCase() === answer.toLowerCase()) indeces.push(i);
     }
-    for (let j = 0; j < word.length; j++) {
+    for (let j = 0; j < word.length; j++) { // builds the new version of finalHiddenWord
         if (indeces.includes(j)) result += answer;
         else if (finalHiddenWord[j] !== '-') result += finalHiddenWord[j];
         else result += '-';
