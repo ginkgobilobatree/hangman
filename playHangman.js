49e6eb6c41a401c0b402cc01e35734d4b2c1e80f
Gino Massalski <gino.massalski@gmail.com>
2022-11-04 23:12:28 +0100

    add english text version

--- playHangman.js
+++ playHangman.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const word = require('./store/wordsStore');
+let word = require('./store/wordsStore');
 const hangman = require('./store/hangmanConstruction');
 const fillHiddenWord = require('./utils/fillHiddenWord');
 const winLoseTimer = require('./utils/winLoseTimer');
@@ -14,61 +14,64 @@ const text = require('./store/text');
 const intervals = require('./store/intervals');
 
 const { log } = console;
+const isEnglish = true;
 
 (() => {
+  word = word.toLowerCase();
   let finalHiddenWord = '-'.repeat(word.length);
   const answers = [];
   let tries = 0;
 
   const recursiveQuestion = () => {
     if (tries === 10) {
-      winLoseTimer(text.loseText(word), intervals.loseTextInterval);
+      winLoseTimer(text.loseText(word, isEnglish), intervals.loseTextInterval);
       runningMan();
       return rl.close();
     }
-    rl.question(tries || answers.length ? text.tryAnotherLetter : text.welcome, (answer) => {
+    rl.question(tries || answers.length ? text.tryAnotherLetter(isEnglish) : text.welcome(isEnglish), (input) => {
+      const answer = input.toLowerCase();
       log(`\n     ${finalHiddenWord}`);
       console.clear();
       // guessed whole word correctly
 
-      if (answer.toLowerCase() === word.toLowerCase()) {
+      if (answer.toLowerCase() === word) {
         console.clear();
-        winLoseTimer(text.winText(word), intervals.winTextInterval);
+        winLoseTimer(text.winText(word, isEnglish), intervals.winTextInterval);
         return rl.close();
       }
 
       // partly correct
 
-      if (word.toLowerCase().includes(answer.toLowerCase()) && answer.length > 0 && answer.toLowerCase() !== answer.toUpperCase()) {
-        answers.includes(answer) ? null : answers.push(answer);
-        log(text.veryGood(answer));
+      if (word.includes(answer) && answer.length > 0 && answer !== answer.toUpperCase()) {
+        if (!answers.includes(answer)) answers.push(answer);
+        log(text.veryGood(answer, isEnglish));
         if (tries > 0) { // builds hangman
           log(hangman[tries - 1]);
         }
         finalHiddenWord = fillHiddenWord(finalHiddenWord, word, answer);
         if (finalHiddenWord.toLowerCase() === word.toLowerCase()) {
           log(`\n     ${word}`);
-          winLoseTimer(text.winText(word), intervals.winTextInterval);
+          winLoseTimer(text.winText(word, isEnglish), intervals.winTextInterval);
           return rl.close();
         }
         log(`\n     ${finalHiddenWord}`);
-        log(text.youTried, ...answers);
+        log(text.youTried(isEnglish), ...answers);
         recursiveQuestion();
       } else if (answer.length > 1 || answer.toLowerCase() === answer.toUpperCase()) {
         console.clear();
-        log(text.onlySingleLetters);
-        answer.length ? answers.push(answer) : null;
+        log(text.onlySingleLetters(isEnglish));
+        if (answer.length) answers.push(answer);
         recursiveQuestion();
       }
 
       // wrong guess
 
       else {
-        answer.length > 0 ? answers.push(answer) : null;
-        log(text.wrongGuess(answer));
+        if (answer.length > 0) answers.push(answer);
+        log(text.wrongGuess(answer, isEnglish));
         log(hangman[tries]); // builds hangman
         log(`\n     ${finalHiddenWord}`);
-        log(text.youTried, ...answers);
+        log(text.youTried(isEnglish), ...answers);
         tries += 1;
         recursiveQuestion();
       }
--- store/text.js
+++ store/text.js
@@ -1,14 +1,14 @@
 const text = {
-  winText: (word, english) => (english ? [`\n     Du hast GEWONNEN!!! DU HAST ${word.toUpperCase()} HERAUSBEKOMMEN!!!`, '\n\n     ... ... ...', '\n\n     Dann wird später trotzdem jemand gehängt.', '\n\n     ... ...', '\n\n     Also hast du trotzdem verloren. HAHAHAHA!!!', '\n\n     ...\n', ''] : [`\n     Du hast GEWONNEN!!! DU HAST ${word.toUpperCase()} HERAUSBEKOMMEN!!!`, '\n\n     ... ... ...', '\n\n     Dann wird später trotzdem jemand gehängt.', '\n\n     ... ...', '\n\n     Also hast du trotzdem verloren. HAHAHAHA!!!', '\n\n     ...\n', '']),
+  winText: (word, isEnglish) => (isEnglish ? [`\n     You WON!!! YOU SOLVED ${word.toUpperCase()}!!!`, '\n\n     ... ... ...', '\n\n     Anyways, someone will have to hang later.', '\n\n     ... ...', '\n\n     So, in the end you lost. HAHAHAHA!!!', '\n\n     ...\n', ''] : [`\n     Du hast GEWONNEN!!! DU HAST ${word.toUpperCase()} HERAUSBEKOMMEN!!!`, '\n\n     ... ... ...', '\n\n     Dann wird später trotzdem jemand gehängt.', '\n\n     ... ...', '\n\n     Also hast du trotzdem verloren. HAHAHAHA!!!', '\n\n     ...\n', '']),
 
-  loseText: (word) => ['\n     Schade!', `\n\n     Du hast nach "${word}" gesucht und leider verloren.`, '', ''],
-  tryAnotherLetter: '\n     Versuche einen anderen Buchstaben! ',
-  welcome: '\n     Willkommen bei Galgenbaum! Errate das Wort, indem du mit einem Buchstaben deiner Wahl beginnst. ',
-  onlySingleLetters: '     Bitte nur Buchstaben und zwar einzeln.',
-  youTried: '\n     Du hast folgene Buchstaben probiert:',
-  tornRope: '\n\n     Oh, der Strick ist gerissen! Somit wird niemand gehängt!!!',
-  veryGood: (answer) => `\n     Sehr gut, '${answer}' steckt im Wort.\n`,
-  wrongGuess: (answer) => `\n     Tja, leider steckt '${answer}' nicht im Wort.\n`,
+  loseText: (word, isEnglish) => (isEnglish ? ['\n     Bummer!', `\n\n     You were looking for "${word}" and lost.`, '', ''] : ['\n     Schade!', `\n\n     Du hast nach "${word}" gesucht und leider verloren.`, '', '']),
+  tryAnotherLetter: (isEnglish) => (isEnglish ? '\n     Try another letter! ' : '\n     Versuche einen anderen Buchstaben! '),
+  welcome: (isEnglish) => (isEnglish ? '\n     Welcome to hangman! Find the hidden word. Start with guessing a letter of your choice. ' : '\n     Willkommen bei Galgenbaum! Errate das Wort, indem du mit einem Buchstaben deiner Wahl beginnst. '),
+  onlySingleLetters: (isEnglish) => (isEnglish ? '     ...only single letters as input...' : '     Bitte nur Buchstaben und zwar einzeln.'),
+  youTried: (isEnglish) => (isEnglish ? '\n     You tried the following input:' : '\n     Du hast schon folgendes ausprobiert:'),
+  tornRope: (isEnglish) => (isEnglish ? '\n\n     Oh, the rope is torn! Thus no one will be hanged!!!' : '\n\n     Oh, der Strick ist gerissen! Somit wird niemand gehängt!!!'),
+  veryGood: (answer, isEnglish) => (isEnglish ? `\n     Very good, '${answer}' is included.\n` : `\n     Sehr gut, '${answer}' steckt im Wort.\n`),
+  wrongGuess: (answer, isEnglish) => (isEnglish ? `\n     Well, too bad that '${answer}' is not included.\n` : `\n     Tja, leider steckt '${answer}' nicht im Wort.\n`),
 };
 
 module.exports = text;
