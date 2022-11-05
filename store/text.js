fb2be0a148eda9a02a3f7afd84f707e1629d0f0e
Gino Massalski <gino.massalski@gmail.com>
2022-11-05 15:03:23 +0100

    get rid of empty code line

--- store/text.js
+++ store/text.js
@@ -1,6 +1,5 @@
 const text = {
   winText: (word, isEnglish) => (isEnglish ? [`\n     You WON!!! YOU SOLVED ${word.toUpperCase()}!!!`, '\n\n     ... ... ...', '\n\n     Anyways, someone will have to hang later.', '\n\n     ... ...', '\n\n     So, in the end you lost. HAHAHAHA!!!', '\n\n     ...\n', ''] : [`\n     Du hast GEWONNEN!!! DU HAST ${word.toUpperCase()} HERAUSBEKOMMEN!!!`, '\n\n     ... ... ...', '\n\n     Dann wird später trotzdem jemand gehängt.', '\n\n     ... ...', '\n\n     Also hast du trotzdem verloren. HAHAHAHA!!!', '\n\n     ...\n', '']),
-
   loseText: (word, isEnglish) => (isEnglish ? ['\n     Bummer!', `\n\n     You were looking for "${word}" and lost.`, '', ''] : ['\n     Schade!', `\n\n     Du hast nach "${word}" gesucht und leider verloren.`, '', '']),
   tryAnotherLetter: (isEnglish) => (isEnglish ? '\n     Try another letter! ' : '\n     Versuche einen anderen Buchstaben! '),
   welcome: (isEnglish) => (isEnglish ? '\n     Welcome to hangman! Find the hidden word. Start with guessing a letter of your choice. ' : '\n     Willkommen bei Galgenbaum! Errate das Wort, indem du mit einem Buchstaben deiner Wahl beginnst. '),
