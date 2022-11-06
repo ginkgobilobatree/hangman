c99e264d336085f4359fa280d386a6012f734b25
Gino Massalski <gino.massalski@gmail.com>
2022-11-06 22:05:48 +0100

    decide for english library

--- store/wordsStore.js
+++ store/wordsStore.js
@@ -1,5 +1,7 @@
-const library = ['Tannenzapfen', 'Baum', 'Blatt', 'Stumpf', 'Ast', 'Krone', 'Wurzel', 'Knospe', 'grün', 'Wald', 'Holz', 'Borke', 'Harz', 'Zweig', 'Spross', 'Rinde', 'Gehölz'];
+const library = ['tree', 'leave', 'stump', 'branch', 'crown', 'root', 'bud', 'green', 'forest', 'wood', 'bark', 'resin', 'twig', 'sprout', 'grove', 'log'];
+// const bibliothek = ['Baum', 'Blatt', 'Stumpf', 'Ast', 'Krone', 'Wurzel', 'Knospe', 'grün', 'Wald', 'Holz', 'Borke', 'Harz', 'Zweig', 'Spross', 'Rinde', 'Gehölz'];
 const randomIndex = Math.floor(Math.random() * library.length);
+
 const word = library[randomIndex];
 
 module.exports = word;
